6d17bc73db74bfe5ca616161970706e5678b2c4d
Mark Castro <markangelcastro@gmail.com>
2025-10-04 16:50:29 -0600

    Add files via upload
    RAD 1st Form - Movie Database *Tanjiro voice

---
+++ Movie Database Application/Form1.cs
@@ -0,0 +1,25 @@
+namespace Movie_Database_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
